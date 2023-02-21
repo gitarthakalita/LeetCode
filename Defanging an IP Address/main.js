fd6e0666deb68baea9694bdebcf97ae18588b629
gitarthakalita <gitarthakalita.prsnl@gmail.com>
2023-02-21 16:29:18 +0530

    wip

--- Defanging an IP Address/main.js
+++ Defanging an IP Address/main.js
@@ -4,9 +4,9 @@ var address = "1.1.1.1";
 
 var defangIPaddr = function(address) {
     
-    // let temp = address.split(".").join("[.]")
+    let temp = address.split(".").join("[.]")
 
-    let temp = address.split();
+    // let temp = address.split();
 
     console.log(temp);
 
